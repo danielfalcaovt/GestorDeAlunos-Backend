1f79d875718c78ea22acfcf2a68b77af21af7e4b
danielfalcaovt <danielfalcaoviktorio@gmail.com>
2024-04-03 22:17:36 -0300

    Code Clean

--- src/model/CRUD/Update/updateStudent.js
+++ src/model/CRUD/Update/updateStudent.js
@@ -12,34 +12,34 @@ async function updateStudent(req, res) {
       cep,
       email,
       parent,
-      phone
+      phone,
     } = req.body;
-    if(!checkIfRequiredParams(student_id)){
+    if (!checkIfRequiredParams(student_id)) {
       return res
-      .status(404)
-      .json({error: "Parâmetros obrigatórios devem ser preenchidos."})
-    };
+        .status(404)
+        .json({ error: "Parâmetros obrigatórios devem ser preenchidos." });
+    }
     let paramCount = 2;
     let params = `student_id = $1`;
     let paramsName = ["student_id"];
     let passedParams = [student_id];
-    
+
     if (first_name) {
       paramsName.push("first_name");
       params += `, first_name = $${paramCount}`;
-      paramCount++
+      paramCount++;
       passedParams.push(first_name);
     }
     if (last_name) {
       paramsName.push("last_name");
       params += `, last_name = $${paramCount}`;
-      paramCount++
+      paramCount++;
       passedParams.push(last_name);
     }
     if (cpf) {
       paramsName.push("cpf");
       params += `, cpf = $${paramCount}`;
-      paramCount++
+      paramCount++;
       passedParams.push(cpf);
     }
     if (module) {
@@ -63,32 +63,32 @@ async function updateStudent(req, res) {
       }
       paramsName.push("module");
       params += `, module = $${paramCount}`;
-      paramCount++
+      paramCount++;
       passedParams.push(moduleToSend);
     }
     if (address) {
       paramsName.push("address");
       params += `, address = $${paramCount}`;
-      paramCount++
+      paramCount++;
       passedParams.push(address);
     }
     if (cep) {
       paramsName.push("cep");
       params += `, cep = $${paramCount}`;
-      paramCount++
+      paramCount++;
       passedParams.push(cep);
     }
     if (email) {
       paramsName.push("email");
       params += `, email = $${paramCount}`;
-      paramCount++
+      paramCount++;
       passedParams.push(email);
     }
     if (parent) {
       console.log(parent);
       paramsName.push("parent");
       params += `, parent = $${paramCount}`;
-      paramCount++
+      paramCount++;
       passedParams.push(parent);
     }
     if (phone) {
@@ -97,39 +97,45 @@ async function updateStudent(req, res) {
       paramCount++;
       passedParams.push(phone);
     }
-    const checkIfStudentAlreadyExist = await query("SELECT * FROM students WHERE student_id::text = $1", [student_id]);
+    const checkIfStudentAlreadyExist = await query(
+      "SELECT * FROM students WHERE student_id::text = $1",
+      [student_id]
+    );
     if (checkIfStudentAlreadyExist.rowCount > 0) {
       const targetStudent = checkIfStudentAlreadyExist.rows[0];
-     const updatedStudent = await query(`UPDATE students SET ${params} WHERE student_id = ($1)::uuid RETURNING *`, passedParams);
-     if (updatedStudent.rowCount > 0) {
-      const student = updatedStudent.rows[0];
-
-      return res
-      .status(200)
-      .json({student:student});
-     }else{
+      const updatedStudent = await query(
+        `UPDATE students SET ${params} WHERE student_id = ($1)::uuid RETURNING *`,
+        passedParams
+      );
+      if (updatedStudent.rowCount > 0) {
+        const student = updatedStudent.rows[0];
+        return res
+        .status(200)
+        .json({ student: student });
+      } else {
+        return res
+          .status(404)
+          .json({
+            error:
+              "Estudante não foi atualizado, revise os parâmetros passados.",
+          });
+      }
+    } else {
       return res
       .status(404)
-      .json({error: "Estudante não foi atualizado, revise os parâmetros passados."});
-     }
-    }else{
-      return res
-      .status(404)
-      .json({error: "Estudante não existente."});
+      .json({ error: "Estudante não existente." });
     }
   } catch (error) {
-    return res
-    .status(500)
-    .json({error:error.message});
-  };
-};
+    return res.status(500).json({ error: error.message });
+  }
+}
 
 function checkIfRequiredParams(first_name, last_name, cpf) {
   if (!first_name && !last_name && !cpf) {
-    return false
-  }else{
-    return true
-  };
-};
+    return false;
+  } else {
+    return true;
+  }
+}
 
 export default updateStudent;
